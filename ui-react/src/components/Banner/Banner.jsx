9ed6eb2cd09fea0cce8aeb98cf517a769af5f2a3
Josse3 <josse.cottenier@hotmail.com>
2019-09-08 19:29:36 +0200

    Added index page with animated titlte

---
+++ ui-react/src/components/Banner/Banner.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Banner.css';
+
+import Title from './Title/Title';
+
+const Banner = () => {
+    return (
+        <div className="banner">
+            <Title />
+        </div>
+    )
+}
+
+export default Banner;
