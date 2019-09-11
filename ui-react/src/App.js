295592219ca7f5e49369bb6235a3446fd7927762
Josse3 <josse.cottenier@hotmail.com>
2019-09-11 15:08:35 +0200

    Imported + displayed new developer page

--- ui-react/src/App.js
+++ ui-react/src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 
 import Banner from './components/Banner/Banner';
+import Developer from './components/Developer/Developer';
 
 function App() {
   return (
     <div className="App">
       <Banner />
+      <Developer />
     </div>
   );
 }
