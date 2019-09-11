18742e6223b92f0e0201c6b77e15af9f4505b9bd
Josse3 <josse.cottenier@hotmail.com>
2019-09-11 20:43:23 +0200

    Added new content to the information card

--- ui-react/src/components/Developer/Card/Card.jsx
+++ ui-react/src/components/Developer/Card/Card.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Card.css';
 
 import developerImg from '../../../images/developer-card/code.png';
@@ -7,39 +7,62 @@ import portfolioImg from '../../../images/developer-card/portfolio.png';
 import contactImg from '../../../images/developer-card/contact.png';
 
 const Card = () => {
-    const icons = [
+    const [currentPage, setCurrentPage] = useState('developer');
+
+    const pages = [
         {
-            text: 'developer',
-            href: developerImg
+            title: 'developer',
+            content: <p>Josse is a full stack developer who works primarily with JavaScript, HTML and CSS (including frameworks React and Vue), NodeJS and PostgreSQL. Besides that, he is in the process of learning Golang.</p>,
+            img: developerImg
         },
         {
-            text: 'personal',
-            href: personImg
+            title: 'personal',
+            content: (
+                <ul>
+                    <li><b>Name:</b> Josse Cottenier</li>
+                    <li><b>Age:</b> {Math.abs(new Date(Date.now() - new Date(2003, 13, 10)).getUTCFullYear() - 1970)}</li>
+                    <li><b>Profession:</b> Student</li>
+                    <li><b>Field of study:</b> Latin and ancient Greek</li>
+                    <li><b>Hobbies:</b> Music (piano and singing), programming</li>
+                </ul>
+            ),
+            img: personImg
         },
         {
-            text: 'portfolio',
-            href: portfolioImg
+            title: 'portfolio',
+            img: portfolioImg
         },
         {
-            text: 'contact',
-            href: contactImg
+            title: 'contact',
+            img: contactImg
         }
     ];
 
     return (
         <div className="card">
             <div className="icon-menu">
-                {icons.map(icon => {
+                {pages.map(page => {
                     return (
-                        <div key={icon.text} className="icon-container">
-                            <img src={icon.href} alt={icon.text} />
-                            <p>{icon.text}</p>
+                        <div
+                            key={page.title}
+                            className={`icon-container ${page.title === currentPage ? 'active' : ''}`}
+                            onClick={() => setCurrentPage(page.title)}
+                        >
+                            <img src={page.img} alt={page.title} />
+                            <p>{page.title}</p>
                         </div>
                     );
                 })}
             </div>
             <div className="content-display">
-
+                {pages.map(page => {
+                    return page.title === currentPage && (
+                        <>
+                            <h1>{page.title}</h1>
+                            {page.content}
+                        </>
+                    );
+                })}
             </div>
         </div>
     )
