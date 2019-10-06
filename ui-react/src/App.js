1fdea072f6a9479745836a1bcc0a8e525189df9f
Josse3 <josse.cottenier@hotmail.com>
2019-10-06 08:25:00 +0200

    Console message

--- ui-react/src/App.js
+++ ui-react/src/App.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 
 import Banner from './components/Banner/Banner';
 import Developer from './components/Developer/Developer';
 
 function App() {
+  useEffect(() => {
+    console.log(
+      'Hi there fellow developer! 👋\nThe source code of this website can be found at https://github.com/Josse3/josse-cottenier. \nHappy forking! 😀'
+    );
+  }, []);
+
   return (
     <div className="App">
       <Banner />
